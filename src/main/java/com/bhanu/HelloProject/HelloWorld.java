f1ecfad56e38cdd1bf03bd9574673f85023bf48e
Shashidhar Pasika <bhanu.molakathalla@acsicorp.com>
2020-10-16 13:42:58 +0530

    Project Updated

--- src/main/java/com/bhanu/HelloProject/HelloWorld.java
+++ src/main/java/com/bhanu/HelloProject/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Welcome to Jenkins.....from Maven");
+		System.out.println("Welcome to Jenkins.....from Maven Project");
 	}
 
 }
